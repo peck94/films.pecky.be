ab74015aae4fccd8c2e51dee009c42d37ee49e9f
pecky <peckjonathan@gmail.com>
2017-02-04 19:34:37 +0100

    Updates

--- app/controllers/films_controller.rb
+++ app/controllers/films_controller.rb
@@ -28,7 +28,7 @@ class FilmsController < ApplicationController
 
     respond_to do |format|
       if @film.save
-        format.html { redirect_to @film, notice: 'Film was successfully created.' }
+        format.html { redirect_to @film, notice: 'De film is aangemaakt.' }
         format.json { render :show, status: :created, location: @film }
       else
         format.html { render :new }
@@ -42,7 +42,7 @@ class FilmsController < ApplicationController
   def update
     respond_to do |format|
       if @film.update(film_params)
-        format.html { redirect_to @film, notice: 'Film was successfully updated.' }
+        format.html { redirect_to @film, notice: 'De film is bijgewerkt.' }
         format.json { render :show, status: :ok, location: @film }
       else
         format.html { render :edit }
@@ -56,7 +56,7 @@ class FilmsController < ApplicationController
   def destroy
     @film.destroy
     respond_to do |format|
-      format.html { redirect_to films_url, notice: 'Film was successfully destroyed.' }
+      format.html { redirect_to films_url, notice: 'De film is verwijderd.' }
       format.json { head :no_content }
     end
   end
