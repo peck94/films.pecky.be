3672a08d47e0b9525f212fecf2c3ecab7f981014
pecky <peckjonathan@gmail.com>
2017-02-04 23:10:47 +0100

    Cache search results

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,19 @@
 class SearchController < ApplicationController
+  @@results_cache = []
+  @@query = nil
+
   def index
-    @query = params[:q]
-    @results = Imdb::Search.new(@query)
+    if @@query.nil? or @@query != params[:q]
+      @query = params[:q]
+      @results = Imdb::Search.new(@query).movies
+
+      @@query = @query.dup
+      @@results_cache = @results.dup
+    else
+      @query = @@query
+      @results = @@results_cache
+    end
+
     @start = params.has_key?(:start) ? params[:start].to_i : 0
     @length = 10
   end
