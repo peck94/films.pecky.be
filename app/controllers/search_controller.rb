b4223789630a22e16586a96eb61ffa8398aa2275
pecky <peckjonathan@gmail.com>
2017-02-04 20:27:28 +0100

    Updates

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,7 +2,7 @@ class SearchController < ApplicationController
   def index
     @query = params[:q]
     @results = Imdb::Search.new(@query)
-    @start = 0
+    @start = params.has_key?(:start) ? params[:start] : 0
     @length = 10
   end
 end
