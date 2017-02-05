ca5ddd318aab7771db6ec4367aaa08b6be530067
pecky <peckjonathan@gmail.com>
2017-02-05 01:59:27 +0100

    Filter movies

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,13 @@
 class MainController < ApplicationController
   def index
-    @films = Film.all
+    if params.has_key?(:commit)
+      @films = Film.find_by_filter(params)
+
+      @gezien = params[:gezien]
+      @rating = params[:rating]
+    else
+      @films = Film.all
+    end
   end
 
   def about
--- app/models/film.rb
+++ app/models/film.rb
@@ -6,4 +6,12 @@ class Film < ApplicationRecord
       errors.add(:rating, 'Je hebt een verkeerd aantal Jodensterren opgegeven.')
     end
   end
+
+  def self.find_by_filter(params)
+    if params[:gezien].to_i == 0
+      return Film.where(rating: params[:rating])
+    else
+      return Film.where(rating: params[:rating], gezien: params[:gezien].to_i == 1)
+    end
+  end
 end
